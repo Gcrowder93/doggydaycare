2a7353a7cd272f8dcc8077ea9f8eeed6fbd43656
Chase Crowder <gcrowder93@gmail.com>
2022-01-05 17:07:46 -0800

    ending right now with a 400 error

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// realizing i dont need this because im mapping in a different folder, but trying to fix that and bring it here brings up more issues
 
-export default function DogCard({ id, name, image, breed }) {
-  return (
-    <div className="dog-card">
-      <Link to={`/dogs/${id}`} key={id}>
-        <h2>{name}</h2>
-        <img alt="dog" src={`${image}`}></img>
-        <p>{breed}</p>
-      </Link>
-    </div>
-  );
-}
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default function DogCard({ id, name, image, breed }) {
+//   return (
+//     <div className="dog-card">
+//       <Link to={`/dogs/${id}`} key={id}>
+//         <h2>{name}</h2>
+//         <img alt="dog" src={`${image}`}></img>
+//         <p>{breed}</p>
+//       </Link>
+//     </div>
+//   );
+// }
